04472ea91c5661de2f53ea6dfb303e0438467a74
amnesiawho <ai28092005@gmail.com>
2025-08-24 21:05:44 +0300

    feat: Added the ability to run the autisticstuff.progress_bars.progress_bar module from the console

--- src/autisticstuff/progress_bars/progress_bar.py
+++ src/autisticstuff/progress_bars/progress_bar.py
@@ -1,44 +1,55 @@
+"""Module with basic progress bar"""
+
+import time
 from collections.abc import Iterable
 from typing import Any
+from sys import stdout
+from autisticstuff.progress_bars.utils import draw_circle_test
 
-from .utils import draw_circle_test
-from .enums import Styles
+from autisticstuff.progress_bars.enums import Styles
+
 
 def progress_bar(
-    iterable: int | Iterable[Any],
-    max_length: int = None,
-    title: str | None = None,
-    main_char: str = "|",
-    space_char: str = ".",
-    size: int = 50,
-    show_percentage: bool = True,
-    show_count: bool = True,
-    style: Styles = "classic",
+        iterable: int | Iterable[Any],
+        max_length: int = None,
+        title: str | None = None,
+        main_char: str = "|",
+        space_char: str = ".",
+        size: int = 50,
+        show_percentage: bool = True,
+        show_count: bool = True,
+        style: Styles = Styles.CIRCLE,
 ):
-        if max_length is None:
-            try:
-                max_length = len(iterable)
-            except TypeError as exc:
-                raise ValueError(
-                    "Если передан итерируемый объект без длины, нужно явно указать max_length"
-                ) from exc
-        for idx, _ in enumerate(iterable, 1):
-            perc_complete = int(idx * 100 / max_length)
-            percent_str = f" ({perc_complete}%)"
-            num_bars = int(perc_complete * size / 100)
-            _progress_bar = "[" + main_char * num_bars + space_char * (size - num_bars) + "]"
-            count_str = f" {idx}/{max_length}"
-            custom_title = "Do something..."
+    if max_length is None:
+        try:
+            max_length = len(iterable)
+        except TypeError as exc:
+            raise ValueError(
+                "Если передан итерируемый объект без длины, нужно явно указать max_length"
+            ) from exc
+    for idx, _ in enumerate(iterable, 1):
+        perc_complete = int(idx * 100 / max_length)
+        percent_str = f" ({perc_complete}%)"
+        num_bars = int(perc_complete * size / 100)
+        _progress_bar = "[" + main_char * num_bars + space_char * (size - num_bars) + "]"
+        count_str = f" {idx}/{max_length}"
+        custom_title = "Do something..."
 
-            match style:
-                case Styles.CLASSIC:
-                    text = f"\r{title if title else custom_title} - {_progress_bar}{count_str if show_count else ''}"
-                case Styles.CIRCLE:
-                    cir = draw_circle_test(size, perc_complete, main_char, space_char, title=f"{percent_str if show_percentage else title}")
-                    text = f"\r{cir}\n\n{title if show_percentage else ""}"
-            yield text
-        return None
+        match style:
+            case Styles.CLASSIC:
+                text = f"\r{title if title else custom_title} - {_progress_bar}{count_str if show_count else ''}"
+            case Styles.CIRCLE:
+                cir = draw_circle_test(size, perc_complete, main_char, space_char,
+                                       title=f"{percent_str if show_percentage else title}")
+                text = f"\r{cir}\n\n{title if show_percentage else ""}"
+        yield text
+    return None
 
 
-
-
+if __name__ == "__main__":
+    # Example
+    for text in progress_bar(range(100), title="Do something...", show_percentage=True, size=10):
+        stdout.write("\r" + text)
+        stdout.flush()
+        time.sleep(0.04)
+    stdout.write("\n")
