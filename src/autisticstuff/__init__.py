52249332fb185bc5f92d5eea9aa85eaba08e6ac9
plassstic <schwartzalexey@yandex.com>
2025-08-26 21:41:08 +0300

    0.1.1b2 - import issues fix

--- src/autisticstuff/__init__.py
+++ src/autisticstuff/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ["progress_bars", "sqlalchemy", "utilities"]
+from . import (
+	logging as astuff_logs,
+	progress_bars as astuff_progbars,
+	sqlalchemy as astuff_sa,
+	utilities as astuff_utils,
+)
+
+
+__all__ = ["astuff_logs", "astuff_progbars", "astuff_sa", "astuff_utils"]
